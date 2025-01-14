00a7ae0208b7769a61832a3b2d8f25db6149f4ba
alessio-civitella <alessio.civitella19@gmail.com>
2025-01-14 17:22:04 +0100

    Feat: movie card

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <h3>{movie.title}</h3>
+      <p>Titolo Originale: {movie.original_title}</p>
+      <p>Lingua: {movie.original_language}</p>
+      <p>Voto: {movie.vote_average}</p>
+    </div>
+  );
+};
+
+export default MovieCard;
